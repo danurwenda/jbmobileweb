c1a382e86d3d85e0e8eaa90f414abdfe49fd8cff
dimas yusuf <dimas.yusuf@polkam.go.id>
2016-11-29 18:51:20 +0700

    bilingual

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -6,34 +6,70 @@ function ValidateEmail(mail) {
     return (false)
 }
 
-$(document).on(
-    'pageshow',
-    '#index',
-    function(e, data) {
-        console.log('wh ' + $(window).height())
-        console.log('oh ' + $('#index-content').outerHeight())
-        $('#index-content')
-            .css('margin-top', ($(window).height() - $('#index-content').outerHeight()) / 2);
+
+var dictionary = {
+    'signup': {
+        'id': 'Daftarkan email anda untuk mendapatkannya GRATIS!',
+        'en': 'Sign up for FREE jBatik Mobile'
     }
+    , 'submit': {
+        'id': 'Daftar',
+        'en': 'Submit'
+    }
+    , 'thank': {
+        'id': 'Terima kasih!',
+        'en': 'Thank you!'
+    }
+    , 'msg': {
+        'id': 'Aplikasi ini dalam proses penyelesaian. Kami akan mengirimkannya lewat email Anda.',
+        'en': 'The apps is in the making. We\'ll let you know when it\'s ready.'
+    }
+}, current_lang = 'id';
+$(function () {
+    $("[data-role='header'], [data-role='footer']").toolbar();
+    translate()
+});
+function translate() {
+    $("[data-translate]").each(function () {
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+}
+$(document).on('tap', '[data-lang]', function () {
+    current_lang = $(this).data('lang');
+    translate()
+})
+
+
+
+$(document).on(
+        'pageshow',
+        '#index',
+        function (e, data) {
+            console.log('wh ' + $(window).height())
+            console.log('oh ' + $('#index-content').outerHeight())
+            $('#index-content')
+                    .css('margin-top', ($(window).height() - $('#index-content').outerHeight()) / 2);
+        }
 );
 
 $(document).on(
-    'tap',
-    '#submit',
-    function(e, data) {
-        /*verify email*/
-        var email = $('#email').val();
-        if (!ValidateEmail(email)) {
-            e.preventDefault();
-        } else {
-          console.log(email);
-            var regis_url = 'http://mobile.jbatik.com/back/index.php/register/register';
-            $.post(regis_url, {
-                email: email
-            }, function(d) {
-                console.log(d)
-                console.log(email);
-            });
+        'tap',
+        '#submit',
+        function (e, data) {
+            /*verify email*/
+            var email = $('#email').val();
+            if (!ValidateEmail(email)) {
+                e.preventDefault();
+            } else {
+                var regis_url = 'http://mobile.jbatik.com/back/index.php/register/register';
+                $.post(regis_url, {
+                    email: email
+                }, function (d) {
+                    console.log(d)
+                    console.log(email);
+                });
+            }
         }
-    }
 );
+
